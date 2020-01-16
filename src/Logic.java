5f35e1d1ab9fbe706172c14facdd97ea15417a49
zainaraza43 <46791180+zainaraza43@users.noreply.github.com>
2020-01-15 22:10:01 -0500

    images / resources class

--- src/Logic.java
+++ src/Logic.java
@@ -3,7 +3,7 @@ import java.awt.event.KeyEvent;
 
 public class Logic {
 	public Logic() {
-
+		new Resources();
 	}
 
 	public void update() {
@@ -15,5 +15,8 @@ public class Logic {
 	public void render(Graphics g) {
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, GameLoop.WIDTH, GameLoop.HEIGHT);
+
+		g.drawImage(Resources.img1, 0, 0, null);
+
 	}
 }
---
+++ src/Resources.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.awt.font.*;
+
+public class Resources {
+
+    public static BufferedImage img1;
+
+    // Loads Resources
+    public Resources() {
+        try {
+            img1 = ImageIO.read(new File(""));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
